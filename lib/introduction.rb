7ff15456077074ba3fe25d61cb2649b4b1999421
Irvin Natt <ipnatt@gmail.com>
2020-11-05 03:07:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,14 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-def introduction_with_language(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
 
   
   
@@ -93,14 +85,3 @@ end
 
 
 
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
